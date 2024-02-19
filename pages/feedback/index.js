f030d8b3d8d89e8bdd4c8312b5b01176a888d7d2
lilyoh <lilyoh628@gmail.com>
2024-02-20 08:29:43 +1100

    change api route structure

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,29 @@
-import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+import { buildFeedbackPath, extractFeedback } from '../api/feedback/feedback';
+import { useState } from 'react';
 
 function FeedbackPage(props) {
+	const [feedbackData, setFeedbackData] = useState();
+
+	function loadFeedbackHandler(id) {
+		fetch(`/api/feedback/${id}`)
+			.then((response) => response.json())
+			.then((data) => {
+				setFeedbackData(data.feedback);
+			});
+	}
+
 	return (
-		<ul>
-			{props.feedbackItems.map((item) => (
-				<li key={item.id}>{item.text}</li>
-			))}
-		</ul>
+		<>
+			{feedbackData && <p>{feedbackData.email}</p>}
+			<ul>
+				{props.feedbackItems.map((item) => (
+					<li key={item.id}>
+						{item.text}
+						<button onClick={loadFeedbackHandler.bind(null, item.id)}>Show Details</button>
+					</li>
+				))}
+			</ul>
+		</>
 	);
 }
 
