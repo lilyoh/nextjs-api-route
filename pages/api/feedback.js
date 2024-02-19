96f2a5b653d161e46a5ffc9d4db97c06267b2520
lilyoh <lilyoh628@gmail.com>
2024-02-19 16:39:15 +1100

    add server side code

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,13 +1,41 @@
+import fs from 'fs';
+import path from 'path';
+
+// 1)
 // react component가 아닌 normal function
 // executed when we visit 'localhost:3000/api/feedback'
 // inside, any server side code is okay
-
 // not going to be bundled to client side code
 // not visible to users
-
 // with this code, we can run our own server side code
+
+// 2)
+// inside of this handler function
+// we have to find out which kind of request is triggering this api route
+// because by default all kinds of requests will trigger this function execution - get, post, put, delete... everything
+
 function handler(req, res) {
-	res.status(200).json({ message: 'this works! ' });
+	if (req.method === 'POST') {
+		const email = req.body.email;
+		const feedbackText = req.body.text;
+
+		const newFeedback = {
+			id: new Date().toISOString(),
+			email: email,
+			text: feedbackText,
+		};
+
+		// store that in a db or in a file
+		const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+		const fileData = fs.readFileSync(filePath);
+		const data = JSON.parse(fileData);
+		data.push(newFeedback);
+		fs.writeFileSync(filePath, JSON.stringify(data));
+
+		res.status(201).json({ message: 'Success!', feedback: newFeedback });
+	} else {
+		res.status(200).json({ message: 'this works! ' });
+	}
 }
 
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,46 @@
+// this frontend code, we should not ask for data - because it's credential, insecure
+// instead, use api route
+// when button is clicked, send a request to api route (to our own api)
+// and in that api route, we can connect to a database
+// because code in api folder will not be exposed
+
+import { useRef } from 'react';
+
 function HomePage() {
+	const emailInputRef = useRef();
+	const feedbackInputRef = useRef();
+
+	function submitFormHandler(event) {
+		event.preventDefault();
+
+		const enteredEmail = emailInputRef.current.value;
+		const enteredFeedback = feedbackInputRef.current.value;
+
+		fetch(); //{ email: 'test@test.com', text: 'some feedback text' }
+	}
+
 	return (
 		<div>
 			<h1>The Home Page</h1>
+			<form onSubmit={submitFormHandler}>
+				<div>
+					<label htmlFor="email">Your Email Address</label>
+					<input
+						type="email"
+						id="email"
+						ref={emailInputRef}
+					/>
+				</div>
+				<div>
+					<label htmlFor="feedback">Your Feedback</label>
+					<textarea
+						id="feedback"
+						rows="5"
+						ref={feedbackInputRef}
+					/>
+				</div>
+				<button>Send Feedback </button>
+			</form>
 		</div>
 	);
 }
